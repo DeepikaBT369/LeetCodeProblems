32388d8b65e06c57cd20ac92c2eab12f5d6a0cb2
DeepikaBT369 <deepikagowdabt@gmail.com>
2026-09-22 12:23:14 -0400

    Time: 50 ms (86.19%) | Memory: 29 MB (62.16%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -5,6 +5,7 @@ class Solution:
         # this is because the window_sum is valid even with the negative , so if I take zero, its not valid for negative numbers
 
         for right in range(k, len(nums)):
+            # we have to start from k, becuase first line ali k k na vargu sum thagondirthivi
             window_sum += nums[right]
             window_sum -= nums[right-k]
             best_sum = max(window_sum, best_sum)
